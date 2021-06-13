6193e735d1f34db5cfa84b1b9a3eb0d32779c7c6
bel-mojo <belmojo.j93@gmail.com>
2021-06-13 12:29:34 -0400

    solar system basic structure is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public class Main {
-    public static void main(String[] args) {
-        int sun;
-        int moon;
-        int planet;
+public class Main extends SolarSystem {
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.sun=1;
+        FactorX.moon=3;
+        FactorX.planet=2;
     }
-}
+
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SolarSystem {
+    public static void main(String[] args) {
+        int sun;
+        int moon;
+        int planet;
+    }
+}
